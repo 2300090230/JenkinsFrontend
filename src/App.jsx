ab6386012dd0062e7555652f941c43c0fa978fc9
Laxman <2300090230@kluniversity.in>
2025-10-12 19:57:16 +0530

    v1.0

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+
+      <h1>Hello Vite + React!</h1>
+      <h2>Start editing to see some magic happen :)</h2>
+      <p>Test paragraph</p>
+      <button type="button" onClick={() => alert('Hello Vite + React!')}>
+        Click me
+      </button>
+
+    </div>
+  )
+}
